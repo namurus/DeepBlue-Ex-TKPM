b3ce5e09eeaaf6172635656af55506de317b98d9
Võ Văn Nam <vovannam050204@gmail.com>
2025-04-04 21:26:15 +0700

    refactor: using react instead of ejs

---
+++ frontend/src/routes/routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DisplayStudentPage from '../pages/DisplayStudentPage';
+
+const routes = [
+    {
+        path: "/",
+        element: <DisplayStudentPage />,
+    },
+];
+
+export default routes;
