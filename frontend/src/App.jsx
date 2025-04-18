a035c1d90bc51713002d8cf0004617849e3cdd9d
Võ Văn Nam <vovannam050204@gmail.com>
2025-04-18 17:03:54 +0700

    feat: add page to display courses

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import DisplayStudentPage from "./pages/DisplayStudentPage";
-import CreateStudentPage from "./pages/CreateStudentPage";
+import AddStudentPage from "./pages/AddStudentPage";
 import UpdateStudentPage from "./pages/UpdateStudentPage";
 import ErrorPage from "./pages/ErrorPage";
+import DisplayCoursePage from "./pages/DisplayCoursePage";
+import AddCoursePage from "./pages/AddCoursePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DisplayStudentPage />} />
-        <Route path="/create-student" element={<CreateStudentPage />} />
+        <Route path="/create-student" element={<AddStudentPage />} />
         <Route path="/update-student" element={<UpdateStudentPage />} />
         <Route path="*" element={<ErrorPage />} />
+        <Route path="/courses" element={<DisplayCoursePage />} />
+        <Route path="/add-course" element={<AddCoursePage />} />
       </Routes>
     </BrowserRouter>
   );
