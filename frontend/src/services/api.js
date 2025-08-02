9740fbc5b6f8b451cdd7636e2ffb6cd5fe6bb6b9
namurus <thaidinhngan@gmail.com>
2025-08-02 23:10:36 +0700

    Change BE API route

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://deepblue-ex-tkpm.onrender.com/',
-    baseURL: 'http://localhost:5134',
+    baseURL: 'https://deepblue-ex-tkpm.onrender.com/',
+    // baseURL: 'http://localhost:5134',
     headers: {
         'Content-Type': 'application/json',
     },
